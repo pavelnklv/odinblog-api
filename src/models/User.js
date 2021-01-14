799a0be041b48c511a0cac030838c71d3880a76b
nikolaev_pavel <nikolaev_pavel@aol.com>
2021-01-14 19:33:52 +0300

    create user model

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  about: {
+    type: String
+  },
+  following: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, { timestamps: true })
+
+module.exports = model('User', userSchema)
