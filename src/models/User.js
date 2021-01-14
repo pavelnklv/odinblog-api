79d018a3a5da05a66e29f89479030b9563c9ee83
nikolaev_pavel <nikolaev_pavel@aol.com>
2021-01-14 20:26:03 +0300

    fix

--- src/models/User.js
+++ src/models/User.js
@@ -3,23 +3,27 @@ const { Schema, model } = require('mongoose')
 const userSchema = new Schema({
   firstName: {
     type: String,
-    required: true
+    required: true,
   },
   lastName: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     unique: true,
-    required: true
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
   },
   about: {
-    type: String
+    type: String,
   },
   following: [{
     type: Schema.Types.ObjectId,
-    ref: 'User'
+    ref: 'User',
   }]
 }, { timestamps: true })
 
