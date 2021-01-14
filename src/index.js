1f866a8b36fec3dc5648d2f5088ed807ecc3b810
nikolaev_pavel <nikolaev_pavel@aol.com>
2021-01-14 20:26:33 +0300

    add usersRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 
+const usersRouter = require('./routers/users')
+
 const app = express()
+
+app.use('/api', usersRouter)
 
 mongoose.connect(process.env.MONGODB_URI, {
   useCreateIndex: true,
