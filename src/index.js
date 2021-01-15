6cb18a95a9db9e8d310c9dbc03c421208b8a365f
nikolaev_pavel <nikolaev_pavel@aol.com>
2021-01-15 18:09:59 +0300

    add articlesRouter

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose')
 
 const usersRouter = require('./routers/users')
 const authRouter = require('./routers/auth')
+const articlesRouter = require('./routers/articles')
 
 const app = express()
 
 app.use('/api', usersRouter)
 app.use('/api', authRouter)
+app.use('/api', articlesRouter)
 
 mongoose.connect(process.env.MONGODB_URI, {
   useCreateIndex: true,
